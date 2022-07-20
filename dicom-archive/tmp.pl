7a84dcf089f673dbf7dfb228933fad8ed03b4f34
cmadjar <cecile.madjar@gmail.com>
2022-07-20 09:52:44 -0400

    bug fix

--- dicom-archive/tmp.pl
+++ dicom-archive/tmp.pl
@@ -1,6 +1,6 @@
 use DICOM::DCMSUM;
 
-my $dcmdir = "/data/incoming/MRI/ARK0000_568842_V01_enhanced/";
+my $dcmdir = "/data/incoming/MRI/ARK0000_568842_V01_enhanced";
 my $dcm_file = "$dcmdir/test_hbcd_enhan.MR.Brain_Adult.7.1.2021.11.09.17.27.35.162.24951006.dcm";
 my $TmpDir = "/data/tmp";
 
