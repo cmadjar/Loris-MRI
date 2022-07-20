74dc924999352b50ede5605a77d4fa09e4c3d34f
cmadjar <cecile.madjar@gmail.com>
2022-07-20 10:33:32 -0400

    modify tmp.p

--- dicom-archive/tmp.pl
+++ dicom-archive/tmp.pl
@@ -1,10 +1,13 @@
 use DICOM::DCMSUM;
+use DICOM::DICOM;
 
 my $dcmdir = "/data/incoming/MRI/ARK0000_568842_V01_enhanced";
 my $dcm_file = "$dcmdir/test_hbcd_enhan.MR.Brain_Adult.7.1.2021.11.09.17.27.35.162.24951006.dcm";
 my $TmpDir = "/data/tmp";
 
-my $summary = DICOM::DCMSUM->new($dcmdir,$TmpDir);
-$summary->read_dicom_data($dcm_file);
+my $dicom = DICOM->new();
+my $fileIsDicom = ! ($dicom->fill($dcm_file));
+
+print($dicom->value('0010','0010'));
 
 exit;
