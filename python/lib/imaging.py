ba38024006b0ac4ec5a5218e2e34d9a2a3088036
cmadjar <cecile.madjar@gmail.com>
2022-07-21 11:14:43 -0400

    add print statements for debugging

--- python/lib/imaging.py
+++ python/lib/imaging.py
@@ -610,13 +610,14 @@ class Imaging:
 
         matching_protocols_list = []
         for protocol in protocols_list:
+            print('\n')
+            print(protocol)
             if protocol['series_description_regex']:
                 if re.search(rf"{protocol['series_description_regex']}", scan_param['SeriesDescription']):
                     matching_protocols_list.append(protocol['Scan_type'])
             elif self.is_scan_protocol_matching_db_protocol(protocol, scan_param):
+                print('in append')
                 matching_protocols_list.append(protocol['Scan_type'])
-
-        print(matching_protocols_list)
 
         return matching_protocols_list
 
