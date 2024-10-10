81f2a5a41fa16f27b31c5126dc95b60fa2cfe20b
cmadjar <cecile.madjar@gmail.com>
2024-10-10 14:47:01 -0400

    add ping

--- python/lib/db/connect.py
+++ python/lib/db/connect.py
@@ -3,6 +3,7 @@ from typing import Any
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy import text
+from urllib.parse import quote
 
 default_port = 3306
 
@@ -14,7 +15,9 @@ def connect_to_db(credentials: dict[str, Any]):
     password = credentials['passwd']
     database = credentials['database']
     port     = int(port) if port else default_port
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@{host}:{port}/{database}', pool_pre_ping=True)
+    print(password)
+    print(quote(password))
+    engine = create_engine(f'mysql+mysqldb://{username}:{quote(password)}@{host}:{port}/{database}', pool_pre_ping=True)
     session = Session(engine)
     session.execute(text("SELECT 1"))
     exit()
