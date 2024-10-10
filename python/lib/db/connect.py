4a08d45d76939399fd2454997427be20975113dd
cmadjar <cecile.madjar@gmail.com>
2024-10-10 14:49:28 -0400

    add ping

--- python/lib/db/connect.py
+++ python/lib/db/connect.py
@@ -14,6 +14,6 @@ def connect_to_db(credentials: dict[str, Any]):
     password = credentials['passwd']
     database = credentials['database']
     port     = int(port) if port else default_port
-    engine = create_engine(f'mysql+mysqldb://{username}:{quote(password)}@{host}:{port}/{database}')
+    engine = create_engine(f'mysql+mysqldb://{username}:{quote(password)}@{host}:{port}/{database}', echo=True)
 
     return Session(engine)
